30789ad5a931e4accb75111568bfcd830ad9dfdf
Anisabil <anisaputriani2603@gmail.com>
2022-10-06 11:02:01 +0700

    warna Kucing

--- warna.java
+++ warna.java
@@ -5,6 +5,8 @@ public class warna{
         System.out.println("-------------");
         System.out.println("1. Putih");
         System.out.println("2. Abu-abu");
+        System.out.println("3. Kuning");
+        System.out.println("4. Hitam");
 
     }
 }
